399f5b101c712b26bc028678e2305df2f1ce42f3
Sean Marquez <capsulecorplab@gmail.com>
2024-04-02 12:03:48 -0700

    hot-fix for rendering photos from assets

--- build.sh
+++ build.sh
@@ -11,6 +11,11 @@ cp -R extras/pdf_shortcodes/static/js/pdf-js static/js/
 if [ ! -r ./node_modules ]; then
     podman run --rm --volume $PWD:/srv -w /srv docker.io/node npm ci
 fi
+
 if [ ! -r ./public ]; then
     podman run --rm --volume $PWD:/src -w /src docker.io/capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine 'hugo --minify -v --destination public'
+fi
+
+if [ ! -r ./content/assets ]; then
+    ln -srv assets content/assets
 fi
